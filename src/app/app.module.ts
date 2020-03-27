2e62724fdd02feee28d23faea6a68ddf6d293ff3
sowngl123 <sowngl123@gmail.com>
2020-03-27 23:34:21 +0700

    1 file modified, 15 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,17 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductAddComponent } from './product-add/product-add.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductEditComponent } from './product-edit/product-edit.component';
+import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, HomeComponent, NotFoundComponent, ProductListComponent, ProductAddComponent, ProductDetailComponent, ProductEditComponent, BannerComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
